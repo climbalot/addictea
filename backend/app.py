89aa69a86e6ef64c858f532b1fa4a8a19a7d5e82
climbalot <florencelry@gmail.com>
2021-12-02 23:43:22 +0800

    folder structure update

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+ma = Marshmallow()
+cors = CORS()
+
+def create_app():
+    app = Flask(__name__)
+    # setup with the configuration provided
+    app.config.from_object('config.DevelopmentConfig')
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # setup all our dependencies
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+    cors.init_app(app)
+    
+    return app
